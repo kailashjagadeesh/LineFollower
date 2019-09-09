e99e2c49395eb061c1f8b38ba2dbf2ff965fe371
Shyam <m.shyam.tnj@gmail.com>
2019-09-09 17:23:26 +0530

    Implement PID logic

--- pid_control/pid_control.cpp
+++ pid_control/pid_control.cpp
@@ -1,7 +1,21 @@
 #include "pid_control.h"
 
-PIDControl::PIDControl(float _kp, float _kd, float _ki) {
+PIDControl::PIDControl(uint16_t& _currentValue, uint16_t& _targetValue, float _kp, float _kd, float _ki):currentValue(_currentValue), targetValue(_targetValue){
     kp = _kp;
     kd = _kd;
     ki = _ki;
+
+    errorSum = 0;
+    lastError = 0;
+}
+
+int16_t PIDControl::control() {
+    int16_t c; //correction
+    int16_t error = currentValue - targetValue;
+    errorSum += error;
+
+    c = kp * error + kd * (error - lastError) + ki * errorSum;
+
+    lastError = error;
+    return c;
 }
--- pid_control/pid_control.h
+++ pid_control/pid_control.h
@@ -3,9 +3,13 @@
 
 class PIDControl {
     float kp, kd, ki;
+    uint16_t& currentValue;
+    uint16_t& targetValue;
+    int16_t errorSum, lastError;
 
     public:
-    PIDControl (float _kp, float _kd, float _ki);
+    PIDControl (uint16_t& _currentValue, uint16_t& _targetValue, float _kp, float _kd, float _ki);
+    int16_t control();
     
 };
 
