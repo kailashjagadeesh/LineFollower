df148c84a24bc662d229f40573e8b91bec4ca34f
Shyam <m.shyam.tnj@gmail.com>
2019-09-14 20:05:52 +0530

    Remove unecessary arguments from QtrSensorsAnalog class

--- QTRSensorsAnalog/QTRSensorsAnalog.cpp
+++ QTRSensorsAnalog/QTRSensorsAnalog.cpp
@@ -1,17 +1,16 @@
 #include <QTRSensors.h>
 #include "QTRSensorsAnalog.h"
 
-QTRSensorsAnalog::QTRSensorsAnalog(const uint8_t* pins, uint8_t numSensors, uint16_t timeout = TIMEOUT):QTRSensors() {
+QTRSensorsAnalog::QTRSensorsAnalog(const uint8_t* pins, uint8_t numSensors):QTRSensors() {
     setTypeAnalog();
     setSensorPins(pins, numSensors);
-    //setEmitterPin(emitterPin);
     //setTimeout(timeout);
 }
 
 uint16_t  QTRSensorsAnalog::readLine(uint16_t* sensorValues) {
-#ifdef LF_BLACKLINE_LOGIC
+#ifndef LF_BLACKLINE_LOGIC
     return readLineBlack(sensorValues);
 #else
-    return readLineWhite(sensorValues);
+    return readLineWhite(sensorValues, QTRReadMode::Off);
 #endif
 }
--- QTRSensorsAnalog/QTRSensorsAnalog.h
+++ QTRSensorsAnalog/QTRSensorsAnalog.h
@@ -3,11 +3,10 @@
 #ifndef QTRSENSORSANALOG_H
 #define QTRSENSORSANALOG_H
 
-#define TIMEOUT 4                      // waits for 2500 us for sensor outputs to go low
 class QTRSensorsAnalog : public QTRSensors{
     public:
 
-    QTRSensorsAnalog(const uint8_t* pins, uint8_t numSensors ,uint16_t timeout = TIMEOUT);
+    QTRSensorsAnalog(const uint8_t* pins, uint8_t numSensors);
     uint16_t readLine(uint16_t *sensorValues);
 };
 
