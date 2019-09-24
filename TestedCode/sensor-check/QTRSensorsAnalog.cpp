3403b2a3886ec7f85f43b9caf4270f2eddd5a0b3
Shyam <m.shyam.tnj@gmail.com>
2019-09-24 17:19:08 +0530

    Tuned PID, reorganised files

--- TestedCode/sensor-check/QTRSensorsAnalog.cpp
+++ TestedCode/sensor-check/QTRSensorsAnalog.cpp
@@ -8,9 +8,9 @@ QTRSensorsAnalog::QTRSensorsAnalog(const uint8_t* pins, uint8_t numSensors):QTRS
 }
 
 uint16_t  QTRSensorsAnalog::readLine(uint16_t* sensorValues) {
-#ifndef LF_BLACKLINE_LOGIC
+#ifdef LF_BLACKLINE_LOGIC
     return readLineBlack(sensorValues);
 #else
-    return readLineWhite(sensorValues, QTRReadMode::Off);
+    return readLineWhite(sensorValues);
 #endif
 }
