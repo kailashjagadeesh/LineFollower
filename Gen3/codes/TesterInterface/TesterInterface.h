a59ff1e55a4c09970aa32fe78bc00e8760a18596
Shyam <m.shyam.tnj@gmail.com>
2019-12-09 16:21:24 +0530

    Easier TestInterface

--- Gen3/codes/TesterInterface/TesterInterface.h
+++ Gen3/codes/TesterInterface/TesterInterface.h
@@ -1,21 +1,13 @@
 #ifndef TESTER_INTERFACE_H
 #define TESTER_INTERFACE_H
 
-#define TEST(s, l) \
+#define TEST(s) \
     void setup() { \
-        runTest(s, sizeof(s)/sizeof(void*)); \
+        s##Setup();\
     } \
 \
     void loop() { \
-        runTest(l, sizeof(l)/sizeof(void*)); \
+        s##Loop();\
     } 
-
-
-typedef void (*testFunction) (void);
-
-void runTest(testFunction funcs[], int n) {
-    for (int i = 0; i < n; i++)
-        funcs[i]();
-}
-
+    
 #endif
