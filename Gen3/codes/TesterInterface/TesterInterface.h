ce7341655fb5a1fb26ad4501d272604608f08944
Shyam <m.shyam.tnj@gmail.com>
2019-12-18 16:17:50 +0530

    Generalize debugging serial

--- Gen3/codes/TesterInterface/TesterInterface.h
+++ Gen3/codes/TesterInterface/TesterInterface.h
@@ -1,6 +1,11 @@
 #ifndef TESTER_INTERFACE_H
 #define TESTER_INTERFACE_H
 
+#ifndef SERIALD 
+//Serial to print debug/error info (TesterInterface)
+#define SERIALD Serial
+#endif
+
 #define TEST(s) \
     void setup() { \
         s##Setup();\
