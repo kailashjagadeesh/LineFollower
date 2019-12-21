4e72fea5d0c89af7ea24e6f4b79b09c16c66d79f
sudharsan2000 <ssudharsan2000@gmail.com>
2019-12-21 21:44:39 +0530

    Implement Ultrasonic::calibrate

--- Gen3/codes/ultrasonic_test/ultrasonic.h
+++ Gen3/codes/ultrasonic_test/ultrasonic.h
@@ -1,13 +1,20 @@
+#ifndef ULTRASONIC_H
+#define ULTRASONIC_H
+#include "PushButtonInterface/PushButtonInterface.h"
+
 class Ultrasonic
 {
     float time;
     float distance;
     int trigPin, echoPin;
+    bool calibrated;
+    int calibratedValue;
 
 public:
     void begin(int, int);
     float measureDistance();
     bool detectBlock(int thresholdDistance);
+    void calibrate();
 };
 // Initializes GPIOs for using ultrasonic
 void Ultrasonic ::begin(int trig = 24, int echo = 22)
@@ -18,6 +25,20 @@ void Ultrasonic ::begin(int trig = 24, int echo = 22)
     pinMode(echoPin, INPUT);
     digitalWrite(trigPin, LOW);
 }
+
+void Ultrasonic::calibrate() {
+    SERIALD.println("Keep the block at appropriate dist.");
+    PushButtonInterface::waitForButton(0);
+
+    float dist = 0;
+    int i;
+    for (i = 0; i < 10; i++)
+        dist += measureDistance();
+
+    calibratedValue = dist / i;
+    calibrated = true;
+}
+
 // Returns distance measured by the sensor in CMs
 float Ultrasonic ::measureDistance()
 {
@@ -35,5 +56,14 @@ float Ultrasonic ::measureDistance()
 //Argument holds threshold distance for deciding if block is present
 bool Ultrasonic :: detectBlock(int thresholdDistance = 5)
 {
-    return ( measureDistance() <= thresholdDistance) ? (1) : (0);
+    if (calibrated)
+        thresholdDistance = calibratedValue;
+
+    return  (measureDistance() <= thresholdDistance) 
+        && (measureDistance() <= thresholdDistance) 
+        && (measureDistance() <= thresholdDistance)
+        && (measureDistance() <= thresholdDistance)
+        && (measureDistance() <= thresholdDistance)
+    ;
 }
+#endif
