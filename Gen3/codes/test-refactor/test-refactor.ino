db7f80c26eaf02ce7b4a6bebe9286112002920cc
sudharsan2000 <ssudharsan2000@gmail.com>
2019-12-31 19:45:27 +0530

    Run tests

--- Gen3/codes/test-refactor/test-refactor.ino
+++ Gen3/codes/test-refactor/test-refactor.ino
@@ -1,18 +1,61 @@
 #include "src/TesterInterface/TesterInterface.h"
+#include "src/AsyncUltrasonic/AsyncUltrasonic.h"
 #include "src/SensorInterface/SensorInterface.h"
+#include "src/MotorDriverInterface/MotorDriverInterface.h"
+#include "src/configure.h"
 #include "src/PushButtonInterface/PushButtonInterface.h"
 
-Sensors sensors; 
+Sensors sensors;
+Motor motors;
 
 void setup() {
     Debug::begin();
     Debug::useBluetooth();
+    // AsyncUltrasonic::init();
+    // AsyncUltrasonic::minimumDistance = ULTRASONIC_MIN_DISTANCE;
 
-    sensors.calibrate();
-    sensors.printCalibratedInfo();
+    // sensors.calibrate();
+    motors.stopMotors();
     PushButtonInterface::waitForButton(0);
+    
 }
 
 void loop() { 
-    Debug::println(sensors.readRearSensors(), 2);
+    // Debug::print(AsyncUltrasonic::distance);
+    // Debug::print(" | ");
+    // Debug::print((int)(AsyncUltrasonic::detectBlock()));
+    // Debug::print(" | ");
+    // sensors.updateAllSensors();
+    // sensors.printAllSensors();
+    // Debug::println("");
+    
+    // AsyncUltrasonic::update();
+
+    motors.setLeftDirection(Motor::FRONT);
+    motors.setRightDirection(Motor::FRONT);
+    motors.setLeftSpeed(255);
+    motors.setRightSpeed(255);
+
+    PushButtonInterface::waitForButton(0);
+
+    motors.setLeftDirection(Motor::FRONT);
+    motors.setRightDirection(Motor::BACK);
+    motors.setLeftSpeed(255);
+    motors.setRightSpeed(255);
+
+    PushButtonInterface::waitForButton(0);
+
+    motors.setLeftDirection(Motor::BACK);
+    motors.setRightDirection(Motor::BACK);
+    motors.setLeftSpeed(255);
+    motors.setRightSpeed(255);
+
+    PushButtonInterface::waitForButton(0);
+
+    motors.setLeftDirection(Motor::BACK);
+    motors.setRightDirection(Motor::FRONT);
+    motors.setLeftSpeed(255);
+    motors.setRightSpeed(255);
+
+    PushButtonInterface::waitForButton(0);
 }
